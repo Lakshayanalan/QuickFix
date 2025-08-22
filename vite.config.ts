d39ee265322e37646be85ee546044728ccd74a7d
Lakshayanalan <alanlakshayan@gmail.com>
2025-08-22 19:35:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  build: {
-    rollupOptions: {
-      input: './index.html',
-    },
-  }
+  base: './',
 });
