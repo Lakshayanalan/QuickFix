f9655f7d375906e0fd754b355b32e911ff94f320
Lakshayanalan <alanlakshayan@gmail.com>
2025-08-22 20:03:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // ✅ make sure Vercel knows where the output is
-    rollupOptions: {
-      input: './index.html',
-    },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
