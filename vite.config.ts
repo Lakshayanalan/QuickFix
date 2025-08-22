6089d23e2032a757df5b8d4e13a52265897cc807
Lakshayanalan <alanlakshayan@gmail.com>
2025-08-22 19:32:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   build: {
     rollupOptions: {
       input: './index.html',
     },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  }
 });
